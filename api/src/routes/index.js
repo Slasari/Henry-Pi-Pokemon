a471cedd4d91c7b66fb4c1732462a7ebc85b1ee4
Slasari <ariel_salcito@live.com>
2022-07-05 14:39:36 -0300

    PI

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -141,14 +141,4 @@ router.delete("/pokemons/:id", async (req, res) => {
   res.status(200).send("Done");
 });
 
-router.put("/pokemons/:id", async (req, res) => {
-  const { name, hp, atk, def, speed, weight, height, type1, type2 } = req.body;
-  try{let pokemonFound = await Pokemon.update({ name, hp, atk, def, speed, weight, height, type1, type2 }, {
-    where: { id: req.params.id },
-  });
-  res.status(200).send("Done")
-} catch{
-  res.status(400).send("Error")
-}});
-
 module.exports = router;
